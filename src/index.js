a7ca871c15318dc1d7f15a60df620fa0e6d83d57
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-06-01 18:59:03 +0100

    json

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 console.log("Hello from NodeJS");
+
+//import inquirer
+const inquirer = require("inquirer");
+
+//What do we want to ask the user?
+
+//Title name
+//Short description of what, why and how
+
+//What is the project for?
+//How do you use it?
+//What are the steps to install your project?
+
+//What was the motivation?
+//Why did you build the project?
+//What problem does it solve?
+//What did you learn?
+
+//How to report issues?
+//How to contribute?
+
+const questions = [
+  {
+    type: "input",
+  },
+];
